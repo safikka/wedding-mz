d2706f5d880094c82b0e43d81d2c0ec5ea985436
mahdy.syafiq <mahdysyafiq51@gmail.com>
2025-08-10 12:23:55 +0700

    [mhdy] new-construct.

--- src/js/postbuild.js
+++ src/js/postbuild.js
@@ -15,3 +15,14 @@ const minified = await minify(html, {
 
 fs.writeFileSync(outputPath, minified);
 console.log('✅ Final HTML (with inline CSS/JS) has been minified.');
+
+// Copy thumbnail
+const source = './public/thumbnail.jpg';
+const dest = './build/thumbnail.jpg';
+
+try {
+  fs.copyFileSync(source, dest);
+  console.log('✅ Thumbnail copied to build/');
+} catch (err) {
+  console.error('❌ Failed to copy thumbnail:', err);
+}
